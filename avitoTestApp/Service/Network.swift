6cd3bac93794ebf4cd34c8f84368e4f158309b08
Ash <ashraf97@yandex.ru>
2021-09-08 22:26:49 +0300

    cached url response

--- avitoTestApp/Service/Network.swift
+++ avitoTestApp/Service/Network.swift
@@ -10,30 +10,37 @@ import Foundation
 struct Network {
     private init() {}
     static let shared = Network()
-    var jsonCache = NSCache<NSString, NSData>()
+    var jsonCache = NSCache<NSString, AnyObject>()
     
     fileprivate let urlString = "https://run.mocky.io/v3/1d1cb4ec-73db-4762-8c4b-0b8aa3cecd4c"
     
     func getData(complition: @escaping(Company) -> ()) {
+        
+        
     guard let url = URL(string: urlString) else {return}
     var request = URLRequest(url: url,timeoutInterval: Double.infinity)
     request.httpMethod = "GET"
-
-    let task = URLSession.shared.dataTask(with: request) { data, response, error in
-        do {
-            guard let data = data else {return}
-            let user = try JSONDecoder().decode(UserModel.self, from: data)
-            DispatchQueue.main.async {
-                complition(user.company)
-              print(user.company.name)
-            }
+        let cc = URLCache.shared
+//        cc.cachedResponse(for: request)
+    
+        let cacheTask = URLSession.shared.dataTask(with: request)
+        cc.getCachedResponse(for: cacheTask) { cache in
+            let data = cache?.data
             
-        } catch {
-            print("Error", error)
+            do {
+                guard let data = data else {return}
+                let user = try JSONDecoder().decode(UserModel.self, from: data)
+                DispatchQueue.main.async {
+                    complition(user.company)
+                  print(user.company.name)
+                }
+                
+            } catch {
+                print("Error", error)
+            }
+            return
         }
-        return
-      }
-    task.resume()
+        cacheTask.resume()
     }
     
 }
